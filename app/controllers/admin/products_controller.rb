1ec547fc19d0c26afefe862018fb6062f67f1121
JacquelineMG <j.gilchrist@outlook.com>
2023-09-25 15:42:30 -0700

    add admin dashboard counts and update order info styling

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: ENV["admin_username"], password: ENV["admin_password"]
+  # http_basic_authenticate_with name: ENV["admin_username"], password: ENV["admin_password"]
 
   def index
     @products = Product.order(id: :desc).all
