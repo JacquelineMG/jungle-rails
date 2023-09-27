1edc875029c9088c105b04687675506e92178476
JacquelineMG <j.gilchrist@outlook.com>
2023-09-27 11:05:33 -0700

    begin implementing user auth -- create user elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  get 'users/create'
   namespace :admin do
     get 'categories/index'
     get 'categories/new'
@@ -28,6 +30,8 @@ Rails.application.routes.draw do
 
 
   resources :about, only: [:index]
+
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
