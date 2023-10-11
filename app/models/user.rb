33bbb6f1863a6a7055ecfb6c09e35b3844de700e
JacquelineMG <j.gilchrist@outlook.com>
2023-10-10 17:19:50 -0700

    implement user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,21 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimum: 5}
   validates :password_confirmation, presence: true
+
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase) 
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+
+  end
+
+
 end 
